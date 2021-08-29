54c5a00dab79cb1472d0e0b5b4b3be9c88508863
omoribp <wellington.omori@neoprospecta.com>
2021-08-29 11:44:57 -0300

    WIP

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # SQL_use_a_cabeca
 
-https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/
-# 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
+
+## Padrçao de commit  
+git commit -m 'Exemplo 001: como criar um banco de dados em PostgreSQL'  
+
+https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/  
+# 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  
---
+++ exemplo002.sql
@@ -0,0 +1,26 @@
+-- Esta é uma linha de comentário em SQL
+-- Este script está sendo construído em POSTGRESQL
+
+-- Comando de criação de tabela lista_donut na página 20 do livro Use a Cabeça SQL
+CREATE TABLE meus_contatos
+(
+	sobre_nome VARCHAR(30),
+	primeiro_nome VARCHAR(20),
+	email VARCHAR(50),
+	aniversario DATE,
+	profissao VARCHAR(50),
+	local VARCHAR(50),
+	estado_civil VARCHAR(20),
+	interesses VARCHAR(100),
+	procura VARCHAR(100)
+);
+
+-- Verificando colunas tabela meus_contatos
+SELECT
+	table_name,
+	column_name,
+	data_type
+FROM
+	information_schema.COLUMNS
+WHERE
+	TABLE_NAME = 'meus_contatos';
