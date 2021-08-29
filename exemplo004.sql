05f0c1e0abac496aa406587599e3e9e8f5a6a44e
omoribp <wellington.omori@neoprospecta.com>
2021-08-29 13:14:24 -0300

    Exemplo 004: comandos DESCRIBE e INSERT

---
+++ exemplo004.sql
@@ -0,0 +1,42 @@
+-- Esta é uma linha de comentário em SQL
+-- Este script está sendo construído em POSTGRESQL
+
+-- Comando de inserção de dados na tabela meus_contatos, conforme descrito na página 33 do livro Use a Cabeça SQL
+-- A tabela meus_contatos CORRETA (com a coluna sexo) foi criada com o script exemplo003.sql
+-- Verificando colunas tabela meus_contatos
+SELECT
+        table_name,
+        column_name,
+        data_type
+FROM
+        information_schema.COLUMNS
+WHERE
+        TABLE_NAME = 'meus_contatos';
+
+-- Inserindo dados na tabela meus_contatos. A ORDEM ENTRE COLUNAS E NOVOS VALORES IMPORTA.
+INSERT INTO meus_contatos
+(
+	sobre_nome,
+	primeiro_nome,
+	email,
+	sexo,
+	aniversario,
+	profissao,
+	local,
+	estado_civil,
+	interesses,
+	procura
+)
+	VALUES
+(
+	'Anderson',
+	'Jillian',
+	'jill_anderson@breakneckpizza.com',
+	'F',
+	'05/09/1980',
+	'Escritora Técnica',
+	'Palo Alto, CA',
+	'Solteiro',
+	'Caiaque, Répteis',
+	'Relacionamentos, Amigos'
+);
