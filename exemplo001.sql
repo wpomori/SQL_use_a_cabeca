d3964d79b887659334aec14ab88698f785533686
omoribp <wellington.omori@neoprospecta.com>
2021-08-29 11:10:40 -0300

    Exemplo 001: como criar um banco de dados em PostgreSQL

---
+++ exemplo001.sql
@@ -0,0 +1,12 @@
+-- Esta é uma linha de comentário em SQL
+-- Este script está sendo construído em POSTGRESQL
+
+-- Comando de criação de tabela lista_donut na página 6 do livro Use a Cabeça SQL
+CREATE TABLE lista_donut
+(
+	nome_donut VARCHAR(10),
+	tipo_donut VARCHAR(8)
+);
+
+-- Verificando colunas tabela lista_donut
+SELECT * FROM lista_donut;
